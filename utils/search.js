8f9877b2b946c43f82ff9c7814fe6298f4633209
saurabh0402 <saurabh.k0402@gmail.com>
2016-06-10 11:36:48 +0530

    Allow user to search youtube and show the results

--- utils/search.js
+++ utils/search.js
@@ -14,6 +14,7 @@ function search(searchQuery){
 	var req = https.request(options, (res) => {
 		res.on('data', (datum) => {
 			html += datum;
+
 		});
 
 		res.on('end', () => {
@@ -24,11 +25,34 @@ function search(searchQuery){
 }
 
 function createResults(html){
-	
+	var $ = cheerio.load(html);
+
+	var videos = $('.yt-lockup-video');
+
+	var i = 0, count = 0;
+	var videoDetails = new Array();
+
+	while(i < videos.length){
+		if(!($('.yt-lockup-byline span.yt-badge-ad', videos[i]).length)){
+			videoDetails[count] = {};
+			videoDetails[count].title = $('.yt-lockup-title a', videos[i]).attr('title');
+			videoDetails[count].link = $('.yt-lockup-title a', videos[i]).attr('href');
+			videoDetails[count].views = $($('.yt-lockup-meta-info li', videos[i])[1]).text();
+			count++;
+		}
+		i++
+	}
+	showResults(videoDetails);
+}
+
+function showResults(videoDetails){
+	count = 1;
+	videoDetails.forEach((item) => {
+		process.stdout.write("\033[31m" + count + ". \033[0m");
+		process.stdout.write(item.title + "   ");
+		console.log("\033[31m- " + item.views + "\033[0m");
+		count++;
+	});
 }
 
 module.exports = search;
-
-//Video-Tile: yt-lockup-video ->
-//Video-Title: yt-lockup-title ->
-//A single a tag, link: href, name: title
