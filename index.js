8d2892816499829d62d3304b7cb05a275a78ab89
saurabh0402 <saurabh.k0402@gmail.com>
2016-06-10 21:32:10 +0530

    Bug fixed - Always sent pgNo as 2 for 2nd request onwards

--- index.js
+++ index.js
@@ -3,14 +3,14 @@ var	searchObj = require('./utils/search');
 process.stdin.resume();
 
 (function main(){
-	var searchQuery;
+	var searchQuery, pgNo = 1;
 	process.stdout.write("Enter Search Term (Type quit to 'exit'): ");
 	process.stdin.once('data', (data) => {
 		searchQuery = data.toString().slice(0, data.length - 1);
 		if(searchQuery.toLowerCase() == 'quit'){
 			process.exit();
 		}
-		searchObj.search(searchQuery, 1);
+		searchObj.search(searchQuery, pgNo);
 	});
 
 	searchObj.on('done', (videoDetails) => {
@@ -20,7 +20,7 @@ process.stdin.resume();
 		process.stdin.once('data', (data) => {
 			choice = Number(data.toString().slice(0, data.length - 1));
 			if(choice > videoDetails.length)
-				searchObj.search(searchQuery, 2);
+				searchObj.search(searchQuery, ++pgNo);
 			else
 				console.log(videoDetails[choice - 1]);
 		});
